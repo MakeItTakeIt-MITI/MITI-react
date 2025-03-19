b1803cc8bc6a6ba96704a6826e434c085d2a5bad
Jiwon Lee <giwon.yi339@gmail.com>
2025-03-19 21:07:36 +0900

    refactor: region filter container height adjusted for mobile view

--- src/features/games/components/RegionFilterContainer.tsx
+++ src/features/games/components/RegionFilterContainer.tsx
@@ -44,7 +44,7 @@ export const RegionFilterContainer = ({
       }}
       className=" overflow-hidden  fixed top-0 right-0 bottom-0 left-0 h-full  z-[999] "
     >
-      <div className="space-y-5 rounded-tl-[20px] rounded-tr-[20px] absolute right-0 bottom-0 left-0 mx-auto sm:w-full md:w-[605px] h-[400px]   bg-secondary-black pt-[30px] px-[21px] pb-[20x] ">
+      <div className=" space-y-5 rounded-tl-[20px] rounded-tr-[20px] absolute right-0 sm:bottom-[33px] md:bottom-0 left-0 mx-auto sm:w-full md:w-[605px] sm:h-[496px] md:h-[400px]   bg-secondary-black pt-[30px] px-[21px] pb-[20px] ">
         <div className="flex justify-between">
           <h1 className="text-base font-bold text-white">지역</h1>
           <button
@@ -55,7 +55,7 @@ export const RegionFilterContainer = ({
           </button>
         </div>
 
-        <ul className="flex items-center  gap-3 flex-wrap">
+        <ul className="flex  gap-3 flex-wrap md:justify-start sm:justify-center ">
           {regions.map((region, i) => (
             <li
               key={i}
@@ -63,7 +63,7 @@ export const RegionFilterContainer = ({
                 border: region === selected ? "1px solid #7FEEF0" : "",
               }}
               onClick={() => handleSelected(region)}
-              className="cursor-pointer w-[103px] h-[48px] p-2.5 bg-light-dark flex items-center justify-center text-white  text-[14px] rounded-lg font-[400]"
+              className="cursor-pointer sm:w-[31%] md:w-[103px] h-[48px] p-2.5 bg-light-dark flex items-center justify-center text-white  text-[14px] rounded-lg font-[400]"
             >
               {region}
             </li>
@@ -81,6 +81,7 @@ export const RegionFilterContainer = ({
           확인
         </button>
       </div>
+      <div className=" md:hidden h-[33px]  sm:absolute bottom-0 left-0 right-0 bg-secondary-black"></div>
     </section>
   );
 };
