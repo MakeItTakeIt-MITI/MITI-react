7686b30a9027f43a6e4066f54cac616673a6e6d0
Jiwon Lee <giwon.yi339@gmail.com>
2025-03-25 19:34:45 +0900

    deploy: hosting guide

--- src/pages/hosting-guide/HostingGuide.tsx
+++ src/pages/hosting-guide/HostingGuide.tsx
@@ -2,8 +2,6 @@ import Footer from "../../components/common/Footer.tsx";
 import { Navbar } from "../../components/navigation/Navbar.tsx";
 
 // import FirstArticle from "../../features/hosting-guide/components/FirstArticle.tsx";
-import SecondArticle from "../../features/hosting-guide/components/SecondArticle.tsx";
-import ThirdArticle from "../../features/hosting-guide/components/ThirdArticle.tsx";
 import FourthArticle from "../../features/hosting-guide/components/FourthArticle.tsx";
 import FirstCarousel from "../../features/hosting-guide/components/FirstCarousel.tsx";
 import SecondCarousel from "../../features/hosting-guide/components/SecondCarousel.tsx";
