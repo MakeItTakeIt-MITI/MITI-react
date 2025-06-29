7eba2cde3a3048552e97db898113855dcf93371b
Jiwon Lee <giwon.yi339@gmail.com>
2025-06-29 20:16:16 +0900

    feat: google/playstore button components and storybook added

---
+++ src/features/common/components(renewal)/buttons/StoreButton.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import StoreButton from "./StoreButton";
+
+const meta: Meta<typeof StoreButton> = {
+  component: StoreButton,
+  title: "Components/Buttons/StoreButton",
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof StoreButton>;
+
+export const ActiveGooglePlayStoreMd: Story = {
+  render: () => <StoreButton size="md" type="google" status="active" />,
+};
+
+export const InactiveGooglePlayStoreMd: Story = {
+  render: () => <StoreButton size="md" type="google" status="inactive" />,
+};
+
+export const ActiveGooglePlayStoreSm: Story = {
+  render: () => <StoreButton size="sm" type="google" status="active" />,
+};
+
+export const InactiveGooglePlayStoreSm: Story = {
+  render: () => <StoreButton size="sm" type="google" status="inactive" />,
+};
+
+export const ActiveAppleStoreMd: Story = {
+  render: () => <StoreButton size="md" type="apple" status="active" />,
+};
+export const InactiveAppleStoreMd: Story = {
+  render: () => <StoreButton size="md" type="apple" status="inactive" />,
+};
+
+export const ActiveAppleStoreSm: Story = {
+  render: () => <StoreButton size="sm" type="apple" status="active" />,
+};
+export const InactiveAppleStoreSm: Story = {
+  render: () => <StoreButton size="sm" type="apple" status="inactive" />,
+};
