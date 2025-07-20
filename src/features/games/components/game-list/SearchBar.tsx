16b2e39564671171f5fbecbb5df1be104ef309a8
Jiwon Lee <giwon.yi339@gmail.com>
2025-07-20 09:34:03 +0900

    fix: UI configured to add left out text

--- src/features/games/components/game-list/SearchBar.tsx
+++ src/features/games/components/game-list/SearchBar.tsx
@@ -7,20 +7,25 @@ export default function SearchBar() {
   console.log(inputContent);
 
   return (
-    <div className="flex items-center w-full ">
-      <div className="flex items-center w-full h-12 border border-[#707070] rounded-[50px] bg-[#141414] px-3">
+    <div className="flex items-center w-full h-12 border border-[#707070] rounded-[50px] bg-[#141414] px-5">
+      {/* Contains input bar */}
+      <div className="flex items-center gap-2.5 w-full">
+        <span className="text-white font-bold ">경기</span>
+        <span className="text-[#707070]">|</span>
         <input
-          className="h-6 rounded-[50px] w-full bg-[#141414] text-white pl-2 outline-none"
+          className="h-6 bg-[#141414] text-white  outline-none"
           type="text"
           onChange={(e) => setInputContent(e.target.value)}
+          placeholder="검색어를 입력해주세요"
         />
-        <button
-          type="button"
-          className="size-6 rounded-full flex items-center justify-center bg-primary-teal ml-3"
-        >
-          <img src={search_icon} alt="" />
-        </button>
       </div>
+      {/* Button to call search API */}
+      <button
+        type="button"
+        className="size-6 rounded-full flex items-center justify-center bg-primary-teal ml-3"
+      >
+        <img src={search_icon} alt="" />
+      </button>
     </div>
   );
 }
