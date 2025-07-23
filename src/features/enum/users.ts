4ad57df4cc8575f1e0b08f52173a03c806978371
Jiwon Lee <giwon.yi339@gmail.com>
2025-07-23 21:58:53 +0900

    feat: enum values and constant added for user information

---
+++ src/features/enum/users.ts
@@ -0,0 +1,36 @@
+
+export enum SignupMethodEnum {
+    EMAIL = 'email',
+    KAKAO = 'kakao',
+    APPLE = 'apple'
+}
+
+export enum PhoneAuthenticationPurposeEnum {
+    SIGNUP = "signup",
+    FINDEMAIL = 'find_email',
+    PASSWORDUPDATE = "password_update"
+}
+
+export enum PlayerPositionEnum {
+    PG = "PG",
+    SG = "SG",
+    SF = "SF",
+    PF = "PF",
+    C = "C",
+}
+
+export enum PlayerRoleEnum {
+    SWINGMAN = "swingman",
+    BALL_HANDLER = "ball_handler",
+    BIGMAN = "bigman",
+    STRETCH_BIGMAN = "stretch_bigman",
+    CONTROL_TOWER = "control_tower",
+    POINT_FORWARD = "point_forward",
+    ALLROUNDER = "allrounder",
+    TWEENER = "tweener",
+    DUAL_GUARD = "dual_guard",
+    SLASHER = "slasher",
+    SHOOTER = "shooter",
+    THREE_AND_D = "3point_and_defense",
+    BLUE_WORKER = "blueworker",
+}
