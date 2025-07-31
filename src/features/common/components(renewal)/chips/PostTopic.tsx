8a9bc1311415e44923a127cae7153c581325f3da
Jiwon Lee <giwon.yi339@gmail.com>
2025-08-01 00:13:52 +0900

    feat: Panel created for community page

---
+++ src/features/common/components(renewal)/chips/PostTopic.tsx
@@ -0,0 +1,7 @@
+export default function PostTopic({ content }) {
+  return (
+    <button className="px-3.5 py-2.5 text-xs text-[#999] rounded-[100px] border border-[#5C5C5C]">
+      {content}{" "}
+    </button>
+  );
+}
--- src/features/community/components/CommunityPanel.tsx
+++ src/features/community/components/CommunityPanel.tsx
@@ -1,5 +1,58 @@
+import PostTopic from "../../common/components(renewal)/chips/PostTopic.tsx";
+
 const CommunityPanel = () => {
-  return <div>CommunityPanel</div>;
+  const topicCategories = [
+    ["자유주제", "코트정보"],
+    ["대회정보", "농구전술"],
+    ["농구화리뷰", "농구팀"],
+    ["팀원 구해요", "게스트후기"],
+    ["해외농구", "국내농구"],
+    ["부상", "농구용품"],
+  ];
+
+  const popularSearches = [
+    ["search word", "서울", "픽업게임"],
+    ["동아리농구방", "MITI", "오늘"],
+  ];
+
+  return (
+    <aside className="flex flex-col gap-6 w-[238px]">
+      {/* 카테고리 */}
+      <div className="flex flex-col gap-4">
+        <h2 className="font-bold text-white">카테고리</h2>
+        <div className="flex flex-col gap-2">
+          {topicCategories.map((group, idx) => (
+            <ul key={idx} className="flex items-center gap-2">
+              {group.map((topic) => (
+                <li key={topic}>
+                  <PostTopic content={topic} />
+                </li>
+              ))}
+            </ul>
+          ))}
+        </div>
+      </div>
+      {/* 인기검색 */}
+      <div className="flex flex-col gap-4">
+        <h2 className="font-bold text-white">인기 검색어</h2>
+        <div className="flex flex-col gap-2">
+          {popularSearches.map((group, idx) => (
+            <ul key={idx} className="flex items-center gap-2">
+              {group.map((topic) => (
+                <li key={topic}>
+                  <PostTopic content={topic} />
+                </li>
+              ))}
+            </ul>
+          ))}
+        </div>
+      </div>
+      {/* 최근 검색어 */}
+      <div className="flex flex-col gap-4">
+        <h2 className="font-bold text-white">최근 검색어</h2>
+      </div>
+    </aside>
+  );
 };
 
 export default CommunityPanel;
