a0a0c0b6906773cf7b4acbdc1b4ef7406c7e4c22
Jiwon Lee <giwon.yi339@gmail.com>
2025-08-07 20:38:41 +0900

    refactor: Modify props to add title to reuse

--- src/features/games/components/game-list/SearchBar.tsx
+++ src/features/games/components/game-list/SearchBar.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
 import search_icon from "../../../../assets/v1.3/icon/search.svg";
 
-export default function SearchBar() {
-  const [inputContent, setInputContent] = useState("");
+interface SearchBarProps {
+  setInputContent: (content: string) => void;
+  title: string;
+}
 
-  console.log(inputContent);
-
+export default function SearchBar({ setInputContent, title }: SearchBarProps) {
   return (
     <div className="flex items-center w-full h-12 border border-[#707070] rounded-[50px] bg-[#141414] px-5">
       {/* Contains input bar */}
       <div className="flex items-center gap-2.5 w-full">
-        <span className="text-white font-bold ">경기</span>
+        <span className="text-white font-bold ">{title}</span>
         <span className="text-[#707070]">|</span>
         <input
           className="h-6 bg-[#141414] text-white  outline-none"
