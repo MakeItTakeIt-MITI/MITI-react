b573ba87f33b23722a53f2818c1a6e914b62aeff
Jiwon Lee <giwon.yi339@gmail.com>
2025-08-17 10:32:59 +0900

    refactor: v1.3 faq page created and added to storybook

---
+++ src/features/faq/components/FaqContainer.tsx
@@ -0,0 +1,5 @@
+const FaqContainer = () => {
+  return <div>FaqContainer</div>;
+};
+
+export default FaqContainer;
--- src/pages/faq/Faq.tsx
+++ src/pages/faq/Faq.tsx
@@ -1,13 +1,13 @@
-import Footer from "../../features/common/components/Footer";
-import Header from "../../features/faq/components/Header";
-import Main from "../../features/faq/components/Main";
+import FaqContainer from "../../features/faq/components/FaqContainer.tsx";
 
 const Faq = () => {
   return (
-    <section className="bg-secondary-black  ">
-      <Header />
-      <Main />
-      <Footer />
+    <section className="w-[800px] min-h-[838px] mx-auto my-10 py-[30px] flex flex-col gap-[36px]">
+      <div className="space-y-5 text-white">
+        <h1 className="text-[32px] font-bold">자주 묻는 질문</h1>
+        <p className="text-[18px] ">궁금하신 내용의 답변을 모아두었어요.</p>
+      </div>
+      <FaqContainer />
     </section>
   );
 };
