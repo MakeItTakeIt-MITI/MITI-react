180963b34b8e3eda91099b086290ef8d7b59b55b
Jiwon Lee <giwon.yi339@gmail.com>
2025-09-30 21:58:47 +0900

    refactor: add transition to selected date  and selected styling

--- src/features/games/components/sidebar/DatesField.tsx
+++ src/features/games/components/sidebar/DatesField.tsx
@@ -28,13 +28,10 @@ export default function DatesField({
         }}
         className="px-4 flex gap-4 overflow-x-auto"
       >
-        <p className="text-[#7FEEF0] flex  items-center justify-center font-bold text-sm w-[37px]">
-          <span>
-            {Number(monthParam) <= 12 && Number(monthParam) > 0
-              ? Number(monthParam)
-              : ""}
-          </span>
-          <span>월</span>
+        <p className="text-[#7FEEF0] font-bold text-sm w-[37px] flex-shrink-0 flex items-center justify-center">
+          {Number(monthParam) <= 12 && Number(monthParam) > 0
+            ? `${Number(monthParam)}월`
+            : ""}
         </p>
         <ul className="flex gap-4 text-[#707070] text-xs font-bold">
           {INITIAL_DATES.map((date: InitialDateField) => {
@@ -43,23 +40,20 @@ export default function DatesField({
             return (
               <li
                 key={dateString}
-                className={`flex flex-col items-center justify-center gap-2 w-[32px] cursor-pointer `}
+                className={`flex flex-col items-center justify-center gap-2 px-1 w-[32px] cursor-pointer `}
                 onClick={() => handleDateClick(date)}
               >
                 <span
                   style={{
                     color: date.dayKorean === "일" ? "#E83E3B" : "",
                   }}
-                  // className={`${
-                  //   isSelected ? "bg-[#1ADCDF] text-white rounded-full" : ""
-                  // }`}
                 >
                   {date.dayKorean}
                 </span>
                 <span
-                  className={`${
-                    isSelected ? "  text-[#1ADCDF] rounded-full" : ""
-                  }`}
+                  className={`rounded-full px-4 py-2 w-full flex items-center justify-center  transition duration-500
+    ${isSelected ? "bg-[#1ADCDF] text-[#000]" : "bg-transparent text-inherit"}
+  `}
                 >
                   {date.date}
                 </span>
