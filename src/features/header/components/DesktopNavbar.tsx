c88e3ade463a51f4aab13c411e3b57a2a26ccd0f
Jiwon Lee <giwon.yi339@gmail.com>
2025-12-21 14:45:40 +0900

    feat: aria accessibility added for web navigation bar

--- src/features/header/components/DesktopNavbar.tsx
+++ src/features/header/components/DesktopNavbar.tsx
@@ -6,11 +6,11 @@ import { isActivePath } from "../utils/isActivePath";
 export default function DesktopNavbar() {
   const { pathname } = useLocation();
 
-  // make navbar transparent only on landing (root) page
   const isLanding = pathname === "/";
 
   return (
     <nav
+      aria-label="웹페이지 기본 내비게이션"
       className={
         (isLanding
           ? "sm:hidden md:flex absolute top-0 left-0 right-0 px-[360px] h-[60px] bg-[#000000D9] items-center justify-center z-40"
@@ -19,47 +19,88 @@ export default function DesktopNavbar() {
       }
     >
       <div className=" h-[28px] w-[1200px]  flex items-center  justify-between">
-        <Link to="/">
-          <img src={navbar_logo} alt="miti logo" />
+        <Link to="/" aria-label="랜딩 페이지로 이동">
+          <img src={navbar_logo} alt="미티 로고" />
         </Link>
-        <ul className="flex items-center gap-5 text-white font-bold text-sm">
-          <li
-            style={{
-              color: isActivePath(pathname, "/games") ? "#1ADCDF" : "white",
-            }}
-          >
-            <Link to={`games`}>경기 목록</Link>
+        <ul
+          className="flex items-center gap-5 text-white font-bold text-sm"
+          role="menubar"
+          aria-label="주요 페이지 이동 메뉴"
+        >
+          <li role="none">
+            <Link
+              role="menuitem"
+              aria-current={
+                isActivePath(pathname, "/games") ? "page" : undefined
+              }
+              to={`games`}
+              style={{
+                color: isActivePath(pathname, "/games") ? "#1ADCDF" : "white",
+              }}
+            >
+              경기 목록
+            </Link>
           </li>
-          <li
-            style={{
-              color: isActivePath(pathname, "/courts") ? "#1ADCDF" : "white",
-            }}
-          >
-            <Link to={`courts?search=`}>경기장 목록</Link>
+          <li role="none">
+            <Link
+              role="menuitem"
+              aria-current={
+                isActivePath(pathname, "/courts") ? "page" : undefined
+              }
+              to={`courts?search=`}
+              style={{
+                color: isActivePath(pathname, "/courts") ? "#1ADCDF" : "white",
+              }}
+            >
+              경기장 목록
+            </Link>
           </li>
-          <li
-            style={{
-              color: isActivePath(pathname, "/community") ? "#1ADCDF" : "white",
-            }}
-          >
-            <Link to="community?search=&category=all">게시판</Link>
+          <li role="none">
+            <Link
+              role="menuitem"
+              aria-current={
+                isActivePath(pathname, "/community") ? "page" : undefined
+              }
+              to="community?search=&category=all"
+              style={{
+                color: isActivePath(pathname, "/community")
+                  ? "#1ADCDF"
+                  : "white",
+              }}
+            >
+              게시판
+            </Link>
           </li>
-          <li
-            style={{
-              color: isActivePath(pathname, "/faq") ? "#1ADCDF" : "white",
-            }}
-          >
-            <Link to={`faq?tab=&search=`}>자주 묻는 질문</Link>
+          <li role="none">
+            <Link
+              role="menuitem"
+              aria-current={isActivePath(pathname, "/faq") ? "page" : undefined}
+              to={`faq?tab=&search=`}
+              style={{
+                color: isActivePath(pathname, "/faq") ? "#1ADCDF" : "white",
+              }}
+            >
+              자주 묻는 질문
+            </Link>
           </li>
-          <li
-            style={{
-              color: isActivePath(pathname, "/inquiries") ? "#1ADCDF" : "white",
-            }}
-          >
-            <Link to={`inquiries?page=1`}>사용자의 문의</Link>
+          <li role="none">
+            <Link
+              role="menuitem"
+              aria-current={
+                isActivePath(pathname, "/inquiries") ? "page" : undefined
+              }
+              to={`inquiries?page=1`}
+              style={{
+                color: isActivePath(pathname, "/inquiries")
+                  ? "#1ADCDF"
+                  : "white",
+              }}
+            >
+              사용자의 문의
+            </Link>
           </li>
         </ul>
-        <AppDownloadBtn />
+        <AppDownloadBtn aria-label="앱 다운로드 버튼" />
       </div>
     </nav>
   );
