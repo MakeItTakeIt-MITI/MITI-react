f5e55ee0177416172b5638471218a4e67e5d7752
Jiwon Lee <giwon.yi339@gmail.com>
2026-01-22 22:49:58 +0900

    feat: lazy loading Hero section for UX improvement

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,10 +1,15 @@
 import "./spline.css";
 import "../../features/landing/style/landing.css";
 import VideoPlaylistContainer from "@/features/landing/components/VideoPlaylistContainer";
-import DesktopHero from "@/features/landing/components/DesktopHero";
+
 import MobileHero from "@/features/landing/components/MobileHero";
 import HostGuideVideoModal from "@/features/landing/components/HostGuideVideoModal";
 import { useLandingPage } from "@/features/landing/hooks/useLandingPage";
+import React, { Suspense } from "react";
+
+const DesktopHero = React.lazy(
+  () => import("@/features/landing/components/DesktopHero")
+);
 
 const Landing = () => {
   const { youtTubeData, openVideo, handleVideoOpen, handleVideoClose } =
@@ -33,7 +38,19 @@ const Landing = () => {
         }}
         className=" relative sm:hidden md:block space-y-20"
       >
-        <DesktopHero handleVideoOpen={handleVideoOpen} />
+        <Suspense
+          fallback={
+            <div className="flex h-[80vh] items-center justify-center">
+              <div
+                className="h-12 w-12 animate-spin rounded-full border-4 border-solid border-white border-t-transparent"
+                role="status"
+                aria-label="로딩 중"
+              ></div>
+            </div>
+          }
+        >
+          <DesktopHero handleVideoOpen={handleVideoOpen} />
+        </Suspense>
         <VideoPlaylistContainer youtubeData={youtTubeData?.items} />
       </main>
     </>
