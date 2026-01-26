3dca333dd84551a90fdfe5ea2943de1757085639
Jiwon Lee <giwon.yi339@gmail.com>
2026-01-26 20:55:30 +0900

    refactor: Adjust height for mobile hero component, add host guide button

--- src/features/landing/components/MobileHero.tsx
+++ src/features/landing/components/MobileHero.tsx
@@ -7,7 +7,7 @@ const MobileHero = () => {
       style={{
         zIndex: 1,
       }}
-      className=" w-full  h-screen"
+      className=" w-full  h-[80vh] relative"
     >
       <Spline
         style={{
@@ -22,7 +22,7 @@ const MobileHero = () => {
       {/* Buttons */}
       <div
         style={{ zIndex: 10 }}
-        className="px-4 absolute bottom-[128px] left-0 right-0 flex flex-col gap-[42px]  justify-center"
+        className="px-4 absolute bottom-[178px] left-0 right-0 flex flex-col gap-[42px]  justify-center"
       >
         <div className="text-white flex flex-col gap-3">
           <h2 className="w-[116px] h-[32px] text-sm bg-[#11AADD33]  text-[#A3F1F2] border border-[#A3F1F2] py-1.5 px-2 flex items-center justify-center rounded-full">
@@ -39,15 +39,29 @@ const MobileHero = () => {
             농구를 즐기는데 필요한 모든 일은 미티가 대신하겠습니다.
           </p>
         </div>
-        <Link
-          to="/games"
-          aria-label="오늘 참여 가능한 경기"
-          className=" mx-auto
-    w-[155px] h-[44px] rounded-full bg-miti-brand text-white font-bold text-sm inline-flex items-center justify-center  
+        <div className="flex items-center gap-3 text-sm">
+          <Link
+            to="/games"
+            aria-label="오늘 참여 가능한 경기"
+            className=" 
+    w-[144px] h-[36px] rounded-full bg-miti-brand text-white  font-medium text-sm inline-flex items-center justify-center  
   "
-        >
-          오늘 참여 가능한 경기
-        </Link>
+          >
+            오늘 참여 가능한 경기
+          </Link>
+
+          <button
+            aria-label="게스트 모집 가이드 영상"
+            className=" 
+    w-[144px] h-[36px] inline-flex items-center justify-center backdrop-blur-md text-white rounded-full border border-miti-brand  font-medium
+       duration-200
+      hover:bg-miti-brand/10 hover:border-miti-brand hover:shadow-sm hover:-translate-y-0.5
+      focus:outline-none focus:ring-4 focus:ring-miti-brandLight/20 
+  "
+          >
+            게스트 모집 가이드
+          </button>
+        </div>
       </div>
     </div>
   );
